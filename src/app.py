46035b63378aee08e3f44658c94eccc0e6b2ea9e
Kushan Mehta <kushan.mehta02@gmail.com>
2019-03-12 21:52:32 +0530

    added docstrings to function for better documentation and reorganize project folders

--- src/app.py
+++ src/app.py
@@ -161,6 +161,10 @@ class CsvEditor(QMainWindow):
         self.action_close_file.triggered.connect(self.close_file)
 
     def load_csv(self):
+        """
+        Loads the file from file selector to a table
+        closes any open file if any before opening new file
+        """
         # Close any already opened file if any
         self.close_file()
 
@@ -226,6 +230,9 @@ class CsvEditor(QMainWindow):
             self.set_bottom_toolbar_info()
 
     def add_blank_data_row(self):
+        """
+        Adds a blank row of data to the table
+        """
         last_row_count = self.csv_data_table.rowCount()
         column_count = self.csv_data_table.columnCount()
         self.csv_data_table.insertRow(last_row_count)
@@ -234,6 +241,9 @@ class CsvEditor(QMainWindow):
             self.csv_data_table.setItem(last_row_count, empty_col, item)
 
     def edit_current_cell(self):
+        """
+        Edits the current cell value by giving a cell entry system similar to MS excel
+        """
         cells = self.csv_data_table.selectionModel().selectedIndexes()
         if len(cells) == 1:
             for cell in sorted(cells):
@@ -242,10 +252,17 @@ class CsvEditor(QMainWindow):
                 self.csv_data_table.editItem(self.csv_data_table.item(r, c))
 
     def delete_selection(self):
+        """
+        Delete the selected cells
+        Automatically determiness if any row or column as a whole is to be deleted
+        :return:
+        """
         # TODO: Add undo, redo etc functionality
         # If whole column is selected remove that column completely
         # If whole row is selected remove that row completely
         # Else make the selected cells blank
+
+        # TODO: Remove the deleted column from the visibility modal also
 
         selected_columns = sorted(self.selected_columns, reverse=True)
         selected_rows = sorted(self.selected_rows, reverse=True)
@@ -281,6 +298,9 @@ class CsvEditor(QMainWindow):
             self.csv_data_table.item(r, c).setText('')
 
     def open_column_layout_dialog(self):
+        """
+        Displays a modal window showing checkboxes for showing the visible columns
+        """
 
         # Create a new instance iff required, else save resources
         if self.column_visibility_dialog_reference is None:
@@ -296,10 +316,17 @@ class CsvEditor(QMainWindow):
         self.hide_invisible_headers()
 
     def set_save_enabled(self, enabled):
+        """
+        Enables the save options in menubar and toolbar
+        :param enabled: The value of whether it is enabled or not depends on this boolean
+        """
         self.action_toolbar_save_file.setEnabled(enabled)
         self.action_save_file.setEnabled(enabled)
 
     def save_file(self):
+        """
+        Saves the file to disk by giving option to select file from file dialog
+        """
 
         file_save_path = QFileDialog.getSaveFileName(self, 'Save CSV', "", 'CSV(*.csv)')
 
@@ -331,6 +358,9 @@ class CsvEditor(QMainWindow):
             QMessageBox.about(self, "Success!", "Your file has been saved successfully.")
 
     def prompt_save_before_closing(self):
+        """
+        Prompt to save the file if any changes are made and the user tries to close the file without saving
+        """
         if self.file_changed:
             choice = QMessageBox.question(self, 'Save File', "Do you want to save file before quiting?",
                                           QMessageBox.Yes | QMessageBox.No)
@@ -338,6 +368,10 @@ class CsvEditor(QMainWindow):
                 self.save_file()
 
     def close_file(self):
+        """
+        Properly close the file and reset all the flags, menu options etc to its original state
+        Also redisplay the start page to the user
+        """
         if self.file_changed:
             self.prompt_save_before_closing()
 
@@ -382,14 +416,21 @@ class CsvEditor(QMainWindow):
         self.set_save_enabled(False)
 
     def closeEvent(self, QCloseEvent):
+        """
+        Gives prompt for saving files if any changes when closing the app directly via the 'X' button at the top toolbar
+        :param QCloseEvent: QT's parameter for handling the closing event
+        """
         # If application is being closed directly prompt for saving modified file
         self.prompt_save_before_closing()
 
     # Helper functions
 
     def cell_change_current(self):
+        """
+         This function is a slot invoked when there is any change in the currently selected cells content
+        """
 
-        # Add exception handling for case when new row is added to unmodified file
+        # Add exception handling for case when new row is added to unmodified file to avoid crash
         try:
             if self.check_cell_change:
                 row = self.csv_data_table.currentRow()
@@ -407,7 +448,10 @@ class CsvEditor(QMainWindow):
                 self.set_save_enabled(True)
 
     def cell_selection_changed(self):
-
+        """
+        This slot gets invoked when the cell selection is changed
+        This also helps us to find out if there is any selection at all and thus enabled delete, edit cell menu options
+        """
         # Enable Edit Cell menu if a single cell is selection else disable it
         self.cells_selected = self.csv_data_table.selectionModel().selectedIndexes()
         if len(self.cells_selected) == 1:
@@ -445,7 +489,11 @@ class CsvEditor(QMainWindow):
             self.set_plot_options(False)
 
     def hide_invisible_headers(self):
-
+        """
+        Helper function for the column visibility modal window
+        core logic for actually hidding a column from view
+        :return:
+        """
         # Hide all the non selected columns
         col_index = 0
         for header in self.column_headers_all:
@@ -458,6 +506,11 @@ class CsvEditor(QMainWindow):
             col_index = col_index + 1
 
     def set_bottom_toolbar_info(self, default_values=False):
+        """
+        Populates the information bar present at the bottom of the app showing info such as
+        column count, row count, selected cells, text length, source file, current selection etc
+        :param default_values: boolean tells us if the file is not yet loaded and hence show the place holder values
+        """
         # Fill the info for the bottom toolbar
         if default_values:
             self.action_toolbar_bottom_column_count.setIconText("Column count -")
@@ -488,7 +541,12 @@ class CsvEditor(QMainWindow):
     # Plot functions
 
     def set_plot_options(self, visibility):
-
+        """
+        The menu bar options for setting the plot
+        These options should only be enabled iff two columns are selected
+        :param visibility: boolean value which sets the options enabled if it is True
+        :return:
+        """
         self.action_toolbar_plot_scatter_points.setEnabled(visibility)
         self.action_toolbar_plot_scatter_points_lines.setEnabled(visibility)
         self.action_toolbar_plot_lines.setEnabled(visibility)
@@ -500,38 +558,58 @@ class CsvEditor(QMainWindow):
         self.action_toolbar_save_plot_png.setEnabled(False)
 
     def plot_scatter_points(self):
+        """
+        Invoke the shared plotting function with parameter 1
+        1 indicates to plot scatter points
+        """
         self.plot(1)
 
     def plot_scatter_points_lines(self):
+        """
+        Invoke the shared plotting function with parameter 2
+        2 indicates to plot scatter points with smooth curve
+        """
         self.plot(2)
 
     def plot_lines(self):
+        """
+        Invoke the shared plotting function with parameter 3
+        2 indicates to plot lines
+        """
         self.plot(3)
 
     def set_plot_title(self):
+        """
+        Sets the plot title when the button is clicked with the value present in the title input box
+        """
         plot_title = self.input_plot_title.text()
         if plot_title:
             self.plot_title = self.input_plot_title.text()
             # Redraw the plot with given title
             if not self.plot_inverted:
-                self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis,
-                               self.plot_inverted)
+                self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis)
             else:
-                self.draw_plot(self.data_y_axis, self.data_x_axis, self.label_y_axis, self.label_x_axis,
-                               self.plot_inverted)
+                self.draw_plot(self.data_y_axis, self.data_x_axis, self.label_y_axis, self.label_x_axis)
         else:
             QMessageBox.about(self, "Error!", "Please enter a title to set in the plot")
 
     def flip_plot_axes(self):
+        """
+        Flips the axes of the current graph
+        meaning X axis becomes Y and vice versa
+        """
         self.plot_inverted = not self.plot_inverted
 
         if not self.plot_inverted:
-            self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis, self.plot_inverted)
+            self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis)
         else:
-            self.draw_plot(self.data_y_axis, self.data_x_axis, self.label_y_axis, self.label_x_axis, self.plot_inverted)
+            self.draw_plot(self.data_y_axis, self.data_x_axis, self.label_y_axis, self.label_x_axis)
 
     def plot(self, plotType):
-
+        """
+        The parent function for setting parameters for plotting and calling the draw function to render the plot
+        :param plotType: defines which type of plot is to be rendered
+        """
         # Build plotting data
         # TODO: Try to improve time complexity of the building up of plotting data
         self.data_x_axis = []
@@ -563,10 +641,16 @@ class CsvEditor(QMainWindow):
         # Set plot type (1,2,3 => order according to scatter, scatter-line, line)
         self.plotType = plotType
 
-        self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis, self.plot_inverted)
+        self.draw_plot(self.data_x_axis, self.data_y_axis, self.label_x_axis, self.label_y_axis)
 
-    def draw_plot(self, data_x_axis, data_y_axis, label_x_axis, label_y_axis, flipped=False):
-
+    def draw_plot(self, data_x_axis, data_y_axis, label_x_axis, label_y_axis):
+        """
+        The core function for the actual plotting which also renders the plot in the tab
+        :param data_x_axis: list having the items of x axis
+        :param data_y_axis: list having the items of y axis
+        :param label_x_axis: text for label for x axis
+        :param label_y_axis: text for label for y axis
+        """
         # Flipped tells us whether to invert the current x, y axis
         self.figure.clear()
 
@@ -613,12 +697,12 @@ class CsvEditor(QMainWindow):
 
                 ax.plot(xnew, power_smooth)
             except:
+                # Switch to normal plot if the data is not purely numeric in which case a smooth curve is not possible
                 ax.plot(data_x_axis, data_y_axis)
 
             # ax.plot(data_x_axis, data_y_axis)
 
         else:
-
             ax.scatter(data_x_axis, data_y_axis)
 
         self.canvas.draw()
@@ -627,7 +711,9 @@ class CsvEditor(QMainWindow):
         self.action_toolbar_save_plot_png.setEnabled(True)
 
     def save_plot_as_png(self):
-
+        """
+        Displays file dialog to save the current plot as png when the save plot as png button is clicked
+        """
         file_save_path = QFileDialog.getSaveFileName(self, 'Save Plot PNG', "", "PNG (*.png)|*.png")
 
         if file_save_path[0]:
@@ -635,6 +721,9 @@ class CsvEditor(QMainWindow):
             QMessageBox.about(self, "Success!", "Your plot has been saved as png image successfully.")
 
     def close_plot_tab(self):
+        """
+        Closes the plot tab when the close tab button is clicked
+        """
         # Temporary tab reference is kept to avoid garbage collection of the UI of tab
         tmp_tab_reference = self.plot_page_tab
         self.tabWidget.removeTab(1)
@@ -653,7 +742,12 @@ class ColumnLayoutDialog(QDialog):
         self.btn_save_header_view.clicked.connect(self.save_header_list)
 
     def add_header_visible_options(self, header_list, visible_list):
-
+        """
+        Populates the checkbox list for all the columns present in the table
+        Also ensures already hidden column's checkbox remains unchecked
+        :param header_list: The list of all the columns in the table
+        :param visible_list: The list of currently visible colummns in the table
+        """
         layout = QVBoxLayout()
         for header in header_list:
             check_box = QCheckBox(header)
@@ -670,6 +764,12 @@ class ColumnLayoutDialog(QDialog):
         self.visible_headers_list = visible_list
 
     def save_header_list(self):
+        """
+        When the OK button is clicked after selecting the columns to show/hide
+        Its state needs to be reflected in the original table as well
+        this function ensures that the reference of the visible list of MainWindow object also gets updated
+        All checked columns are added to the list present with the above class's visible_column_list
+        """
         self.visible_headers_list.clear()
 
         check_box_list = self.column_layout_list_scroll_area.findChildren(QCheckBox)
@@ -679,6 +779,7 @@ class ColumnLayoutDialog(QDialog):
 
 
 if __name__ == '__main__':
+    # Run the application
     app = QApplication(sys.argv)
     window = CsvEditor()
     sys.exit(app.exec_())
